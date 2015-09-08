780a404627ca2068a58945b189bf70a0023f99dc
Evelyn Hathaway <git@evelynhathaway.com>
2015-09-07 17:30:20 -0700

    Extension, W3 Schools Draft 1

---
+++ js/content_script.js
@@ -0,0 +1,16 @@
+// doc = document;
+// URL = doc.URL;
+// if (/(http(s)?:\/\/)?(www)?\.w3schools\.com\/.+/i.test(URL)) {
+// 	var style = document.createElement('link');
+// 	style.rel = 'stylesheet';
+// 	style.type = 'text/css';
+// 	style.href = chrome.extension.getURL('/css/www.w3schools.com.css');
+// 	(document.head||document.documentElement).appendChild(style);
+// } else {
+
+// };
+// // alert("Hello");
+// // console.log("Hello");
+// // Waves.attach('#topnavbtn_tutorials');
+// // Waves.init();
+// // Waves.ripple('#topnavbtn_tutorials');
